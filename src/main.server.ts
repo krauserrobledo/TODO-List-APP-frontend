2506bb6e6243f1520e65d7c97f60c8f1b6576037
krauserrobledo <krauser.robledo@gmail.com>
2025-11-05 12:11:02 +0100

    chore(structure): configure initial structure

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,6 @@
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { App } from './presentation/app/app';
+import { config } from './presentation/app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
     bootstrapApplication(App, config, context);
