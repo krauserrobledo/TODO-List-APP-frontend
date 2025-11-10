c416028f8914c824120406937c6f6047dcee66ed
krauserrobledo <krauser.robledo@gmail.com>
2025-11-10 14:42:53 +0100

    fix(register): fixed page Language

--- src/domain/repositories/auth-repository.ts
+++ src/domain/repositories/auth-repository.ts
@@ -1,4 +1,3 @@
-import { Injectable } from '@angular/core';
 import { AuthResponse, LoginRequest, RegisterRequest, ValidateTokenRequest } from '../entities/auth-entity';
 import { UserEntity } from '../entities/user-entity';
 
